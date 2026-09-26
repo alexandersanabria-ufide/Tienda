c44274577a8f6938f6ccf1bc090e032bb968689b
alexandersanabria <alexandersanabria@10.211.55.8>
2026-09-25 21:06:29 -0600

    Commit materia - clase 3

---
+++ src/main/java/com/tienda/tienda/controller/CategoriaController.java
@@ -0,0 +1,38 @@
+package com.tienda.tienda.controller;
+
+import com.tienda.tienda.domain.Categoria;
+import com.tienda.tienda.service.CategoriaService;
+import jakarta.validation.Valid;
+import java.util.Locale;
+import java.util.Optional;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+    private final MessageSource messageSource;
+
+    public CategoriaController(CategoriaService categoriaService, MessageSource messageSource) {
+        this.categoriaService = categoriaService;
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
+    }
+}
